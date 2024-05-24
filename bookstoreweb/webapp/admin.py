523e097b2b9b4045cdf551508a6b6a9795245061
huynt119 <22022509@vnu.edu.vn>
2024-05-25 01:22:52 +0700

    update admin

--- bookstoreweb/webapp/admin.py
+++ bookstoreweb/webapp/admin.py
@@ -17,8 +17,16 @@ class TagAdmin(admin.ModelAdmin):
 class UserAccountAdmin(UserAdmin):
     model = UserAccount
 
+class TransactionAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+class BookQuantityAdmin(admin.ModelAdmin):
+    search_fields = ['book__title']
+
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
 admin.site.register(UserAccount, UserAccountAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Rating, RatingAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(BookQuantity, BookQuantityAdmin)
